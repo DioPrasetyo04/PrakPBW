ce4cf5b746add4722900a72b1e2d8b2538f64040
DioPrasetyo04 <prasetyodio04@gmail.com>
2024-10-18 13:07:06 +0700

    add auth

---
+++ course_laravel/storage/framework/views/6eb553577e48725ce8ae1e15fe162977.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <p> Halaman contact</p>
+    <ul>
+        <li><a href="https://www.instagram.com/dioolinger0406/" target="blank">Instagram</a></li>
+    </ul>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blogger</a>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\course_laravel\resources\views/contact.blade.php ENDPATH**/ ?>
